9560cba07be426d3eddcd78cd805ff79e1c68271
RyanROCKET-YYH <116342145+RyanROCKET-YYH@users.noreply.github.com>
2023-09-09 02:35:10 -0400

    convert turtle direction to enum

--- student/yuhongy_student_turtle.cpp
+++ student/yuhongy_student_turtle.cpp
@@ -30,12 +30,13 @@ float z, aend, mod, bp, q;
 // the maze!)
 
 enum TurtleOrientation {
-	turn_right = 0,
-	turn_left = 1,
-	move_forward = 2,
+	west = 0,
+	south = 1,
+	east = 2,
+	north = 3,
 };
 
-bool studentMoveTurtle(QPointF &pos_, int &nw_or) {
+bool studentMoveTurtle(QPointF &pos_, TurtleOrientation &nw_or) {
   ROS_INFO("Turtle update Called  w=%f", w);
   mod = true;
   if (w == 0) {
@@ -44,14 +45,14 @@ bool studentMoveTurtle(QPointF &pos_, int &nw_or) {
     fx2 = pos_.x();
     fy2 = pos_.y();
     if (nw_or < 2) { // magic number what does nw_or point to at 0,1,2,3 
-      if (nw_or == 0)
+      if (nw_or == west)
         fy2 += 1; //
-      else
+      else    // when nw_or = 1, fx += 1
         fx2 += 1; //
     } else {
       fx2 += 1;
       fy2 += 1;
-      if (nw_or == 2)
+      if (nw_or == east)
         fx1 += 1;
       else
         fy1 += 1;
@@ -60,39 +61,39 @@ bool studentMoveTurtle(QPointF &pos_, int &nw_or) {
 	ROS_INFO("bumped?: %s", bp ? "ture" : "false");
     aend = atend(pos_.x(), pos_.y()); // check if arrvies at end (boolean)
 	ROS_INFO("at end?: %s", aend ? "ture" : "false");
-    if (nw_or == 0) {
-      if (cs == 2) {
-        nw_or = 3;
+    if (nw_or == west) {
+      if (cs == 2) {	// not sure what makes cs means and nw_or=3 didn't happened
+        nw_or = north;    // cs = 2, turn right
         cs = 1;
-      } else if (bp) {
-        nw_or = 1;
+      } else if (bp) {    //  if bumped, cs becomes 0 turtle turn left
+        nw_or = south;
         cs = 0;
       } else
         cs = 2;
-    } else if (nw_or == 1) {
+    } else if (nw_or == south) {    // 
       if (cs == 2) {
-        nw_or = 0;
+        nw_or = west;    // turn right
         cs = 1;
       } else if (bp) {
-        nw_or = 2;
+        nw_or = east;    // bumped, turn left which is east
         cs = 0;
       } else
         cs = 2;
-    } else if (nw_or == 2) {
+    } else if (nw_or == east) {
       if (cs == 2) {
-        nw_or = 1;
+        nw_or = south;    // turn right
         cs = 1;
       } else if (bp) {
-        nw_or = 3;
+        nw_or = north;    // bumped, turn left which is north
         cs = 0;
       } else
         cs = 2;
-    } else if (nw_or == 3) {
+    } else if (nw_or == north) {
       if (cs == 2) {
-        nw_or = 2;
+        nw_or = east;    // turn right
         cs = 1;
       } else if (bp) {
-        nw_or = 0;
+        nw_or = west;    // turn left, west
         cs = 0;
       } else
         cs = 2;
@@ -101,14 +102,14 @@ bool studentMoveTurtle(QPointF &pos_, int &nw_or) {
     z = cs == 2;
     mod = true;
     if (z == true && aend == false) {
-      if (nw_or == 1)
-        pos_.setY(pos_.y() - 1);
-      if (nw_or == 2)
-        pos_.setX(pos_.x() + 1);
-      if (nw_or == 3)
-        pos_.setY(pos_.y() + 1);
-      if (nw_or == 0)
-        pos_.setX(pos_.x() - 1);
+      if (nw_or == south)
+        pos_.setY(pos_.y() - 1);    // move south when nw_or = 1
+      if (nw_or == east)
+        pos_.setX(pos_.x() + 1);    // move east when nw_or = 2
+      if (nw_or == north)
+        pos_.setY(pos_.y() + 1);    // move north when nw_or = 3
+      if (nw_or == west)
+        pos_.setX(pos_.x() - 1);    // move west when nw_or = 0
       z = false;
       mod = true;
     }
