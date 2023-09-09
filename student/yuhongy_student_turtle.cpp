5356910881334415b460d6eedd307eb63133660d
RyanROCKET-YYH <116342145+RyanROCKET-YYH@users.noreply.github.com>
2023-09-09 06:27:47 -0400

    update right hand rule

--- student/yuhongy_student_turtle.cpp
+++ student/yuhongy_student_turtle.cpp
@@ -43,108 +43,142 @@ enum TurtleState {
 };
 
 bool studentMoveTurtle(QPointF &pos_, int &nw_or) {
-  ROS_INFO("Turtle update Called  w=%f", wait);
+	ROS_INFO("Turtle update Called  w=%f", wait);
   
-  mod = true;
-  if (wait == 0) {
-    fx1 = pos_.x();
-    fy1 = pos_.y(); // initialize the position and used to check for bump
-    fx2 = pos_.x();
-    fy2 = pos_.y();
-    if (nw_or == north || nw_or == east) {
-      nw_or == north ? fy2++ : fx2++;
-    } else {
-      fx2 += 1;
-      fy2 += 1;
-      nw_or == south ? fx1++ : fy1++;
-    }
+  	mod = true;
+  	if (wait == 0) {
+		fx1 = pos_.x();
+		fy1 = pos_.y(); // initialize the position and used to check for bump
+		fx2 = pos_.x();
+		fy2 = pos_.y();
+    	if (nw_or == north || nw_or == east) {
+      		nw_or == north ? fy2++ : fx2++;
+    	} else {
+      		fx2 += 1;
+			fy2 += 1;
+			nw_or == south ? fx1++ : fy1++;
+    	}
 
-    bp = bumped(fx1, fy1, fx2, fy2);  // see if there is a bump (boolean)
-	//ROS_INFO("bumped?: %s", bp ? "ture" : "false");
-    aend = atend(pos_.x(), pos_.y()); // check if arrvies at end (boolean)
-	//ROS_INFO("at end?: %s", aend ? "ture" : "false");
-	ROS_INFO("Current state: %f, Orientation: %d", cs, nw_or);
-	switch(nw_or) {
-        case north:
-			cs == moving_forward ? (nw_or = west, cs = turned_forward) :
-			bp ? (nw_or = east, cs = turned_bumped) : cs = moving_forward;
-            break;
-		
-		case east:
-			cs == moving_forward ? (nw_or = north, cs = turned_forward) :
-			bp ? (nw_or = south, cs = turned_bumped) : cs = moving_forward;
-			break;
+		bp = bumped(fx1, fy1, fx2, fy2);  // see if there is a bump (boolean)
+		//ROS_INFO("bumped?: %s", bp ? "ture" : "false");
+		aend = atend(pos_.x(), pos_.y()); // check if arrvies at end (boolean)
+		//ROS_INFO("at end?: %s", aend ? "ture" : "false");
+		ROS_INFO("Current state: %f, Orientation: %d", cs, nw_or);
+		/*switch(nw_or) {
+			case north:
+				cs == moving_forward ? (nw_or = west, cs = turned_forward) :
+				bp ? (nw_or = east, cs = turned_bumped) : cs = moving_forward;
+				break;
+			
+			case east:
+				cs == moving_forward ? (nw_or = north, cs = turned_forward) :
+				bp ? (nw_or = south, cs = turned_bumped) : cs = moving_forward;
+				break;
 
-		case south:
-			cs == moving_forward ? (nw_or = east, cs = turned_forward) :
-			bp ? (nw_or = west, cs = turned_bumped) : cs = moving_forward;
-			break;
+			case south:
+				cs == moving_forward ? (nw_or = east, cs = turned_forward) :
+				bp ? (nw_or = west, cs = turned_bumped) : cs = moving_forward;
+				break;
 
-		case west:
-			cs == moving_forward ? (nw_or = south, cs = turned_forward) :
-			bp ? (nw_or = north, cs = turned_bumped) : cs = moving_forward;
-			break;
-	}
+			case west:
+				cs == moving_forward ? (nw_or = south, cs = turned_forward) :
+				bp ? (nw_or = north, cs = turned_bumped) : cs = moving_forward;
+				break;
+		}*/
+		// right hand rule
+		switch(nw_or) {
+			case north:
+				cs == moving_forward ? (nw_or = east, cs = turned_forward) :
+				bp ? (nw_or = west, cs = turned_bumped) : cs = moving_forward;
+				break;
+			
+			case east:
+				cs == moving_forward ? (nw_or = south, cs = turned_forward) :
+				bp ? (nw_or = north, cs = turned_bumped) : cs = moving_forward;
+				break;
+
+			case south:
+				cs == moving_forward ? (nw_or = west, cs = turned_forward) :
+				bp ? (nw_or = east, cs = turned_bumped) : cs = moving_forward;
+				break;
+
+			case west:
+				cs == moving_forward ? (nw_or = north, cs = turned_forward) :
+				bp ? (nw_or = south, cs = turned_bumped) : cs = moving_forward;
+				break;
+		}
 	
-	/*	
-    if (nw_or == north) {
-      if (cs == moving_forward) {	// intend to move forward
-        nw_or = west;    // turn left 0->3 is turning left
-        cs = turned_forward;    // recently turned
-      } else if (bp) {    // if bumped, means can't move foward
-        nw_or = east;    // since left hand rule, it will turn right, 0->1 turning right
-        cs = turned_bumped;    // turned and bumped
-      } else
-        cs = moving_forward;    //intend to move forward
-    } else if (nw_or == east) {   
-      if (cs == moving_forward) {
-        nw_or = north;  
-        cs = turned_forward;
-      } else if (bp) {
-        nw_or = south;    
-        cs = turned_bumped;
-      } else
-        cs = moving_forward;
-    } else if (nw_or == south) {
-      if (cs == moving_forward) {
-        nw_or = east;    
-        cs = turned_forward;
-      } else if (bp) {
-        nw_or = west;    
-        cs = turned_bumped;
-      } else
-        cs = moving_forward;
-    } else if (nw_or == west) {
-      if (cs == moving_forward) {
-        nw_or = south;    
-        cs = turned_forward;
-      } else if (bp) {
-        nw_or = north;    
-        cs = turned_bumped;
-      } else
-        cs = moving_forward;
-    }*/
-	ROS_INFO("Orientation=%f  STATE=%f", nw_or, cs);
-	z = cs == 2;
-    mod = true;
-    if (z == true && aend == false) {    // when intend to move forward
-      if (nw_or == east)
-        pos_.setY(pos_.y() - 1);    // or = 1, turn left (y-1), or = 1 is east
-      if (nw_or == south)
-        pos_.setX(pos_.x() + 1);    // south nw_or = 2, x+1
-      if (nw_or == west)
-        pos_.setY(pos_.y() + 1);    // east nw_or = 3, y+1
-      if (nw_or == north)
-        pos_.setX(pos_.x() - 1);    // north nw_or = 0, x-1
-      z = false;
-      mod = true;
-    }
-  }
-  if (aend)
-    return false; // don't submit change if reaches destination
-  if (wait == 0)
-    wait = TIMEOUT; // countdown to 0 and reset to TIMEOUT
-  else
-    wait -= 1;
-  return wait == TIMEOUT;
+		/*	
+		if (nw_or == north) {
+		if (cs == moving_forward) {	// intend to move forward
+			nw_or = west;    // turn left 0->3 is turning left
+			cs = turned_forward;    // recently turned
+		} else if (bp) {    // if bumped, means can't move foward
+			nw_or = east;    // since left hand rule, it will turn right, 0->1 turning right
+			cs = turned_bumped;    // turned and bumped
+		} else
+			cs = moving_forward;    //intend to move forward
+		} else if (nw_or == east) {   
+		if (cs == moving_forward) {
+			nw_or = north;  
+			cs = turned_forward;
+		} else if (bp) {
+			nw_or = south;    
+			cs = turned_bumped;
+		} else
+			cs = moving_forward;
+		} else if (nw_or == south) {
+		if (cs == moving_forward) {
+			nw_or = east;    
+			cs = turned_forward;
+		} else if (bp) {
+			nw_or = west;    
+			cs = turned_bumped;
+		} else
+			cs = moving_forward;
+		} else if (nw_or == west) {
+		if (cs == moving_forward) {
+			nw_or = south;    
+			cs = turned_forward;
+		} else if (bp) {
+			nw_or = north;    
+			cs = turned_bumped;
+		} else
+			cs = moving_forward;
+		}*/
+		ROS_INFO("Orientation=%f  STATE=%f", nw_or, cs);
+		z = cs == 2;
+		mod = true;
+		/*if (z == true && aend == false) {    // when intend to move forward
+			if (nw_or == east)
+				pos_.setY(pos_.y() - 1);    // or = 1, turn left (y-1), or = 1 is east
+			if (nw_or == south)
+				pos_.setX(pos_.x() + 1);    // south nw_or = 2, x+1
+			if (nw_or == west)
+				pos_.setY(pos_.y() + 1);    // east nw_or = 3, y+1
+			if (nw_or == north)
+				pos_.setX(pos_.x() - 1);    // north nw_or = 0, x-1
+			z = false;
+			mod = true;
+		}*/
+
+		if (z == true && aend == false) {    // when intend to move forward
+			if (nw_or == east)
+				pos_.setY(pos_.y() + 1);    // or = 1, turn right (y+1), or = 1 is east
+			if (nw_or == south)
+				pos_.setX(pos_.x() - 1);    // south nw_or = 2, x-1
+			if (nw_or == west)
+				pos_.setY(pos_.y() - 1);    // west nw_or = 3, y-1
+			if (nw_or == north)
+				pos_.setX(pos_.x() + 1);    // north nw_or = 0, x+1
+			z = false;
+			mod = true;
+		}
+  	}
+	if (aend) return false; // don't submit change if reaches destination
+  	if (wait == 0) {
+		wait = TIMEOUT;
+	} else wait -= 1;
+
+	return wait == TIMEOUT;
 }
