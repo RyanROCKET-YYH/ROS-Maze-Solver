eff749595770f47a10215723a874bfaea660ae5d
RyanROCKET-YYH <116342145+RyanROCKET-YYH@users.noreply.github.com>
2023-10-31 23:17:24 -0400

    test

--- student/yuhongy_student_turtle.cpp
+++ student/yuhongy_student_turtle.cpp
@@ -83,7 +83,7 @@ TurtleOrientation NextMove(TurtleOrientation currentDir, int32_t (&visitCounts)[
 
 	int32_t minvisitCount = INT32_MAX;
 	TurtleOrientation nextDir = error;
-	for (int8_t i = 4; i > 0; i--) {
+	for (int8_t i = 0; i < 4; i++) {
 		TurtleOrientation direction = priorityOrder[currentDir][i];
 		int32_t count = 0;
 		bool hasWall = false;
@@ -108,7 +108,7 @@ TurtleOrientation NextMove(TurtleOrientation currentDir, int32_t (&visitCounts)[
 				ROS_ERROR("Invalid orientation");
 				break;
 		}
-		if (!hasWall && count <= minvisitCount) {
+		if (!hasWall && count < minvisitCount) {
 			minvisitCount = count;
 			nextDir = direction;
 		}
