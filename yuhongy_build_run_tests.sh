b9e0937cbf9762684a2f809e4d3facad6d43ada2
RyanROCKET-YYH <116342145+RyanROCKET-YYH@users.noreply.github.com>
2023-11-12 23:30:00 -0500

    yuhongy_build_run_tests.sh

---
+++ yuhongy_build_run_tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd student
+
+COMPILER=g++
+STD=-std=c++11
+CPPFLAGS=-Dtesting
+OUTPUT=-o yuhongy_student_tests
+SOURCES="yuhongy_student_tests.cpp yuhongy_student_turtle.cpp yuhongy_mock_functions.cpp"
+LIBRARIES=-lcunit
+
+$COMPILER $STD $CPPFLAGS $OUTPUT $SOURCES $LIBRARIES
+
+if [ $? -eq 0 ]; then
+    # Run the tests
+    ./yuhongy_student_tests
+
+    exit $?
+else
+    echo "Compilation failed."
+    exit 1
+fi
