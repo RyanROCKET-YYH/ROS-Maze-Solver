cca568f8059be7bb8fc72e870ca582ed4add4928
RyanROCKET-YYH <116342145+RyanROCKET-YYH@users.noreply.github.com>
2023-11-12 23:03:09 -0500

    2222

--- student/yuhongy_student_test.cpp
+++ student/yuhongy_student_test.cpp
@@ -847,7 +847,10 @@ int main() {
         (NULL == CU_add_test(pSuite, "test of transition S5 -> S3", test_t16_2)) ||
         (NULL == CU_add_test(pSuite, "test of transition S5 -> S3", test_t17)) ||
         (NULL == CU_add_test(pSuite, "test of default turtleState case", test_t18)) ||
-        (NULL == CU_add_test(pSuite, "test of NextMove subroutine", test_NextMove)))
+        (NULL == CU_add_test(pSuite, "test of NextMove subroutine", test_NextMove))
+        (NULL == CU_add_test(pSuite, "test of NextMove subroutine", test_NextMove_1)) ||
+        (NULL == CU_add_test(pSuite, "test of NextMove subroutine", test_NextMove_2)) ||
+        (NULL == CU_add_test(pSuite, "test of NextMove subroutine", test_NextMove_3)))
 
 
     {
