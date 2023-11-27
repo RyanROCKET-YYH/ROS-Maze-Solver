e6b13a0b2f0796bf6a19ee9b9d17d6b0a26237c5
RyanROCKET-YYH <116342145+RyanROCKET-YYH@users.noreply.github.com>
2023-11-27 17:12:05 -0500

    Add face monitor

---
+++ monitors/yuhongy_face_monitor.cpp
@@ -0,0 +1,90 @@
+/*
+ * Author: Yuhong Yao 
+ * AndrewID: yuhongy
+ *
+ * This monitor ensures the turtle do not check the wall bump coindition in the wrong direction
+ * In my implementation, everytime I receive a pose, the bumpInterrupt will be called
+ * so that I can check if the turtle is checking the wall in the forward direction
+ * by comparing the endpoints of the wall segment based on the current pose and orientation
+ * with the endpoints of the wall segment from bumpInterrupt
+ */
+#ifdef testing
+#include "yuhongy_mock.h"
+#endif
+#ifndef testing
+#include "monitor_interface.h"
+#endif
+
+// get the current pose of the turtle from poseInterrupt
+static Pose curr_pose;
+static Endpoints curr_endpoints;
+static Orientation curr_orientation;
+
+/*
+ * get the current pose of the turtle
+ */
+void poseInterrupt(ros::Time t, int x, int y, Orientation o) {
+  curr_orientation = o;
+  curr_pose.x = x;
+  curr_pose.y = y;
+}
+
+/*
+ * get the endpoints of the turtle based on its current position and orientation
+ */
+Endpoints getEndpoints(int x, int y, Orientation o) {
+  switch(o) {
+    case NORTH:
+      curr_endpoints.x1 = x;
+      curr_endpoints.y1 = y;
+      curr_endpoints.x2 = x + 1;
+      curr_endpoints.y2 = y;
+      break;
+    case SOUTH:
+      curr_endpoints.x1 = x;
+      curr_endpoints.y1 = y + 1;
+      curr_endpoints.x2 = x + 1;
+      curr_endpoints.y2 = y + 1;
+      break;
+    case EAST:
+      curr_endpoints.x1 = x + 1;
+      curr_endpoints.y1 = y;
+      curr_endpoints.x2 = x + 1;
+      curr_endpoints.y2 = y + 1;
+      break;
+    case WEST:
+      curr_endpoints.x1 = x;
+      curr_endpoints.y1 = y;
+      curr_endpoints.x2 = x;
+      curr_endpoints.y2 = y + 1;
+      break;
+    default:
+      ROS_ERROR("Unexpected value for turtle's direction");
+      break;
+  }
+  return curr_endpoints;
+}
+
+// make sure the turtle is checking the wall in the forward direction
+void bumpInterrupt(ros::Time t, int x1, int y1, int x2, int y2, bool bumped) {
+    // Print bump info
+    curr_endpoints = getEndpoints(curr_pose.x, curr_pose.y, curr_orientation);
+    ROS_INFO("[[%ld ns]] 'Bump' was sent. Data: x1 = %d, y1=%d, x2=%d, y2=%d, bumped=%d", t.toNSec(), x1, y1, x2, y2, bumped);
+    // Check if the turtle is moving across the wall
+    if (!(curr_endpoints.x1 == x1 && curr_endpoints.y1 == y1 && curr_endpoints.x2 == x2 && curr_endpoints.y2 == y2)) {
+      ROS_WARN("VIOLATION: Turtle is not checking the wall in the forward orientation");
+    }
+}
+
+/*
+ * Empty interrupt handlers beyond this point
+ */
+
+void tickInterrupt(ros::Time t) {
+}
+
+void visitInterrupt(ros::Time t, int visits) {
+}
+
+void atEndInterrupt(ros::Time t, int x, int y, bool atEnd) {
+}
