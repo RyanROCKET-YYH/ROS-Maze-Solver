50297ddfc1d1e65cbfbb0d5d14e3b6a44d61ccac
RyanROCKET-YYH <116342145+RyanROCKET-YYH@users.noreply.github.com>
2023-11-27 18:06:34 -0500

    Add new monitor for end of program

---
+++ monitors/yuhongy_atend_monitor.cpp
@@ -0,0 +1,57 @@
+/*
+ * Author: Yuhong Yao 
+ * AndrewID: yuhongy
+ *
+ * This monitor ensures that the turtle does not search for the end of the maze
+ * it can only call atend for its current position
+ * 
+ */
+
+#include "monitor_interface.h"
+
+// Keeps track of the last pose received
+static Pose curr_pose;
+static bool solved = false;
+static bool poseUpdated = false; // flag that indicates if the pose has been updated    
+
+/*
+ * Update the current pose of the turtle
+ */
+void poseInterrupt(ros::Time t, int x, int y, Orientation o) {
+    curr_pose.x = x;
+    curr_pose.y = y;
+    poseUpdated = true;
+}
+
+/*
+ * Check if the turtle is calling the atend function for its current position
+ */
+void atEndInterrupt(ros::Time t, int x, int y, bool atEnd) {
+    if (poseUpdated && (curr_pose.x != x || curr_pose.y != y)) {
+        ROS_WARN("VIOLATION: Turtle is not calling atend based on current location!");
+    }
+
+    if (atEnd) {
+        ROS_WARN("Successful at end of the maze");
+        solved = true;
+    }
+    poseUpdated = false;
+}
+
+void tickInterrupt(ros::Time t) {
+    // Optionally flush warnings every tick if needed
+    if (solved) {
+        ROS_WARN("Successful at end of the maze");
+    }
+}
+
+/*
+ * Empty interrupt handlers beyond this point
+ */
+
+void visitInterrupt(ros::Time t, int visits) {
+}
+
+void bumpInterrupt(ros::Time t, int x1, int y1, int x2, int y2, bool bumped) {
+}
+
